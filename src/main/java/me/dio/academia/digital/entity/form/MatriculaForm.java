99599433f6c9fec3f2564e7470019e0aae1a19bd
rafaelds9 <rafaelds57@gmail.com>
2022-08-20 12:06:45 -0300

    Added matricula

--- src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
@@ -1,5 +1,13 @@
 package me.dio.academia.digital.entity.form;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MatriculaForm {
 
   private Long alunoId;
